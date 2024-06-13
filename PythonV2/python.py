93844e686d3044fa47cd07e45cd3bc967c39be99
Maikon Barbosa <maikon.barbosa@largoinc.com>
2024-06-12 21:35:12 -0300

    Proz-html-css

---
+++ PythonV2/python.py
@@ -0,0 +1,8 @@
+produtos_musicais = ["Violão", "Teclado", "Bateria", "Flauta", "Microfone"]
+
+# Array com anos de nascimento de familiares e amigos
+anos_nascimento = [1985, 1990, 1995, 2000, 2005]
+
+# Imprimindo os arrays
+print("Produtos musicais:", produtos_musicais)
+print("Anos de nascimento:", anos_nascimento)
